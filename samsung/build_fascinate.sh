3db19c68276281c0376bc2e3c3183717100ffd3a
codeworkx <daniel.hillenbrand@codeworkx.de>
2011-02-19 06:33:54 -0800

    added samsung fascinate

---
+++ samsung/build_fascinate.sh
@@ -0,0 +1,26 @@
+#/bin/bash 
+ 
+case "$1" in 
+     clean) 
+          make clean 
+          rm -rf ./out 
+          exit 
+          ;; 
+     prepare) 
+          cd vendor/cyanogen 
+          ./get-rommanager
+          ./get-google-files
+          exit
+          ;;
+     otapackage) 
+          . build/envsetup.sh 
+          lunch cyanogen_fascinate-eng
+          make -j`grep 'processor' /proc/cpuinfo | wc -l` otapackage
+	  exit
+          ;; 
+     *)
+esac
+
+. build/envsetup.sh 
+lunch cyanogen_fascinate-eng
+make -j`grep 'processor' /proc/cpuinfo | wc -l`
